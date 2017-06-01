b84fbaf2ea99c591896eb091846d959074932335
Julien Castiaux <julien.castiaux@gmail.com>
2017-06-01 18:24:54 +0200

    remove files, leave only the readme, the schemas and the wiki

--- julien/flask/sql/init.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE product(product_id serial PRIMARY KEY, name varchar(16) UNIQUE, stock int);
-INSERT INTO product (name, stock) VALUES ('poupee', 5), ('chateau', 15), ('petite voiture', 50);
-
